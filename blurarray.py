1728f6b410faf6068e871b65a73ae707e4a9dd31
Handkep <paulus.handke@gmail.com>
2022-05-16 13:48:35 +0200

    dev

--- blurarray.py
+++ blurarray.py
@@ -1,21 +1,22 @@
 import math
-array = [0,0,0,0,10,0,0,0,0]
+array = [300,0,0,0,10,0,50,0,0]
 blurredarray=[]
 print(array)
 mask = [1,2,4,2,1]
 def _avgarr(arrsize,masksize,i):
-    i=i-int(masksize/2)
-    avg = 0
+    indexWithMaskRadius = i-int(masksize/2)
+    total = 0
     count = 0
     for j in enumerate(mask):
-        arrindex = i+j[0]
+        # print(j)
+        arrindex = indexWithMaskRadius+j[0]
         if(arrindex<0):
             continue
-        if(arrindex>arrsize):
+        if(arrindex>arrsize-1):
             continue
         count+=1
-        avg += j[1]*array[arrindex-1]
-    blurredarray.append(int(avg/count))
+        total += j[1]*array[arrindex]
+    blurredarray.append(int(total/count))
 
 
 
@@ -23,6 +24,5 @@ def _avgarr(arrsize,masksize,i):
 # print(int(5/2))
 # print(mask[int(5/2)])
 for i in enumerate(array):
-
     _avgarr(9,5,i[0])
 print(blurredarray)
