598d4069130ad0aec80a9a1314c3d9de36f52147
Handkep <paulus.handke@gmail.com>
2022-06-13 20:33:11 +0200

    dev

--- blurarray.py
+++ blurarray.py
@@ -1,12 +1,16 @@
 import math
 # array = [300,0,0,0,10,0,50,0,0,0,0,0,500,0,20,10,3,5]
-array = [[255,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,255,255],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,255,0]]
-blurredarray=[[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
-mask = [1,1,2,2,4,8,4,2,2,1,1]
-# mask = [1,2,3,4,5,5,9,5,5,4,3,2,1]
+array =[[0,0,0]]
+blurredarray=[[]]
+for i in range(100):
+    array.append([0,0,0])
+    blurredarray.append([])    
+# array = [[255,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,255,255],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,255,0]]
+# blurredarray=[[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+# mask = [1,1,2,2,4,8,4,2,2,1,1]
+mask = [1,2,3,4,5,5,9,5,5,4,3,2,1]
 # mask = [1,3,6,9,6,3,1]
 # blurredarray=[]
-print(array)
 
 def printrgb(r,g,b,obj):
     print(f"\x1b[38;2;{r};{g};{b}m{obj}\x1b[0m",end="")
@@ -59,10 +63,11 @@ def _avgarr(arrsize,masksize,i):
 # print(mask[int(5/2)])
 # for i in enumerate(array):
 #     _avgarr(18,5,i[0])
-_blurarr(array,mask,blurredarray)
-for i in blurredarray:
-    printrgb(i[0],i[1],i[2],"X")
-print()
-print(blurredarray)
-print(len(array))
-print(len(blurredarray))
+for j in range(50):
+    print(j)
+    array[j] = [255,255,0]
+    _blurarr(array,mask,blurredarray)
+    for i in array:
+        printrgb(i[0],i[1],i[2],"X")
+    print()
+    array[j] = [0,0,0]
